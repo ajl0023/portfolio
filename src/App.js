9e84a60b90145640f0932bcb75c9895516f80963
austin <austinlee199723@yahoo.com>
2021-03-26 21:39:08 -0700

    edit file routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { CSSTransitionGroup } from "react-transition-group"; // ES6
 import Navbar from "./navbar/Navbar";
 import About from "./about/About";
 import style from "./app.module.scss";
-import "./app.css";
+import "./App.css";
 const App = () => {
   return (
     <div className="app-wrapper">
